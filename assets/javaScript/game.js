c014358c134c75c892bbd7fd26a2cb60c77d2c20
Filip Ratkovic <ratkovic.filip@yahoo.com>
2019-11-24 11:25:29 -0500

    ready for grade small changes2

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -11,7 +11,7 @@ var userLetter = [];
       
 document.getElementById("user-submit").onclick = function(){ 
    
-    if(userLetter.length==1){
+    if(userLetter.length==11){
          return alert("you lost !");
         
 }
@@ -69,6 +69,7 @@ function updateLosses() {
 
      
   }
+  
 
 
 
