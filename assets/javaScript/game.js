500bdbbf007e0b7d06afa64243f643083e0150ea
Filip Ratkovic <ratkovic.filip@yahoo.com>
2019-11-24 10:48:04 -0500

    ready for grade

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,19 +1,80 @@
-var userGuess;
 
-var randchar = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 1);
+var y = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 1);
+console.log(y)
+var wins = 0; 
+var losses= 0;
+var guessLeft = 10;
+var guessesSoFar ="";
+var userLetter = [];
 
-console.log(randchar);
 
-function playGame(){
-
- userGuess = document.getElementById("user-guess").value;
-
- if(userGuess === randchar) {
-    alert ("Yay!");
- }
- else if (usersGuess !== randchar){
-     alert ("Guess again !")
- }
- else alert ("That is not the letter")
-
+      
+document.getElementById("user-submit").onclick = function(){ 
+   
+    if(userLetter.length==10){
+         return alert("you lost !");
+        
 }
+var x = document.getElementById("user-guess").value; 
+
+
+if(x === y) 
+{     
+   alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "); 
+   wins++;
+   updateWins();
+   updateGuessesLeft();
+   updateGuesses();
+   reset();
+} 
+else if(x !== y)  
+              
+{     
+   
+   
+   alert("OOPS SORRY!! TRY Again");
+   losses++;
+   updateLosses();
+   updateGuesses();
+   updateGuessesLeft();
+   reset();
+   
+} 
+}
+
+function updateLosses() {
+    document.querySelector("#loses").innerHTML = "Losses: " + losses;
+  }
+  function updateWins() {
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+  }
+
+  function updateGuessesLeft() {
+    document.querySelector("#guessLeft").innerHTML = "Guesses Left: " + guessLeft;
+    guessLeft--;
+  }
+
+  function updateGuesses() {
+      
+    var guesses2 = document.getElementById("user-guess").value;
+    console.log(guesses2)
+    userLetter.push(guesses2);
+     document.querySelector("#guessSoFar").innerHTML = "Guesses so far: " + userLetter.join(' ');
+     
+  }
+
+  function reset() {
+      
+    document.getElementById('user-guess').value=null;
+
+     
+  }
+
+
+
+  updateLosses();
+  updateWins();
+  updateGuessesLeft();
+  updateGuesses();
+  reset();
+  
